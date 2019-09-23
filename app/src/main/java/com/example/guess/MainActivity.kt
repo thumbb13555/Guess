2de722773c5758a95d3102d4c821915a9152f271
NoahLiu <thumbb13555@gmail.com>
2019-09-23 22:05:21 +0800

    作業一

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -2,6 +2,12 @@ package com.example.guess
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.textclassifier.TextLinks
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +15,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linear_main)
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity","secret"+secretNumber.secret)
+    }
+
+    fun check(view :View){
+        val n = ed_number.text.toString().toInt()
+        Log.d("MainActivity","number: $n")
+        val diff = secretNumber.validate(n)
+        var message = "Yes,you got it"
+        if(diff <0){
+            message = "Bigger"
+        }else if(diff>0){
+            message = "Smaller"
+        }
+//        Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+            AlertDialog.Builder(this)
+                .setTitle("Message")
+                .setMessage(message)
+                .setPositiveButton("OK",null)
+                .show()
     }
 }
